1d654fcdd535b2a52346dd577af963cfcc09b34b
Denis <presetslr@gmail.com>
2019-12-05 00:34:48 +0300

    feat: add solutions
    regexp, objects

--- src/05-regex-tasks.js
+++ src/05-regex-tasks.js
@@ -54,7 +54,7 @@ function getRegexForGuid() {
  *
  */
 function getRegexForPitSpot() {
-  throw new Error('Not implemented');
+  return /^([sr]|pi).*/;
 }
 
 
@@ -78,8 +78,8 @@ function getRegexForPitSpot() {
  *   'PASSW0RD'.match(validator)  => false
  *   'Pa55'.match(validator) => false
  */
-function getPasswordValidator(/* minLength */) {
-  throw new Error('Not implemented');
+function getPasswordValidator( minLength ) {
+  return new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z0-9]{'+minLength+',}');
 }
 
 
--- src/06-objects-tasks.js
+++ src/06-objects-tasks.js
@@ -35,8 +35,8 @@ function Rectangle(/* width, height */) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON( obj ) {
+  return JSON.stringify(obj);
 }
 
 
