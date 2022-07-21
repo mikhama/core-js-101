7bdfe50d64b6629bbdbd732e66ca0c70f99e32a8
dab10 <dab10@list.ru>
2022-07-22 02:05:55 +0300

    feat: finish some array tasks

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -233,8 +233,13 @@ function toArrayOfSquares(arr) {
  *   [ 0, 0, 0, 0, 0]         => [ 0, 0, 0, 0, 0]
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
-function getMovingSum(/* arr */) {
-  throw new Error('Not implemented');
+function getMovingSum(arr) {
+  let prevSum = 0;
+
+  return arr.map((value) => {
+    prevSum = value + prevSum;
+    return prevSum;
+  });
 }
 
 /**
@@ -248,8 +253,8 @@ function getMovingSum(/* arr */) {
  * [ 'a', 'b', 'c' , null ]  => [ "b", null ]
  * [ "a" ] => []
  */
-function getSecondItems(/* arr */) {
-  throw new Error('Not implemented');
+function getSecondItems(arr) {
+  return arr.filter((value, index) => index % 2 !== 0);
 }
 
 
