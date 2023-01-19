1d5e0b7b7da59de45319db523803a0e6a1bf5e8d
baranova <baranova.epam@gmail.com>
2023-01-19 21:43:40 +0200

    Changes of task Core-js

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -233,6 +233,7 @@ function toArrayOfSquares(arr) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
 function getMovingSum(/* arr */) {
+  // return arr.reduce((acc, item, i) => [...acc, item + acc[i === 0 ? 0 : i - 1]], []);
   throw new Error('Not implemented');
 }
 
--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -23,8 +23,8 @@
  *   getComposition(Math.sin, Math.asin)(x) => Math.sin(Math.asin(x))
  *
  */
-function getComposition(/* f, g */) {
-  throw new Error('Not implemented');
+function getComposition(f, g) {
+  return (x) => f(g(x));
 }
 
 
@@ -44,8 +44,8 @@ function getComposition(/* f, g */) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  return (base) => base ** exponent;
 }
 
 
