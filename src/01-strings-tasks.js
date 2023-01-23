fbdd85e31fc2a3aa5fd0da968adafa0d4ca1ae19
Ilona Goldmane <67143829+ilonaGold@users.noreply.github.com>
2023-01-23 23:03:17 +0200

    feat: add concatenate strings task solution

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -18,8 +18,8 @@
  *   'aa',''    => 'aa'
  *   '',  'bb'  => 'bb'
  */
-function concatenateStrings(/* value1, value2 */) {
-  throw new Error('Not implemented');
+function concatenateStrings(value1, value2) {
+  return value1 + value2;
 }
 
 
