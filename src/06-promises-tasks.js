faad2c71cfb906411ac8c36d4ecac0b0b0cf07fd
iMac <imac@iMac-iMac.local>
2023-02-08 22:02:20 +0300

    Test1

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -28,7 +28,7 @@
  *      .catch((error) => console.log(error.message)) // 'Error: Wrong parameter is passed!
  *                                                    //  Ask her again.';
  */
-function willYouMarryMe(/* isPositiveAnswer */) {
+function willYouMarryMe(/* isPositiveAnswer */) { // eslint-
   throw new Error('Not implemented');
 }
 
