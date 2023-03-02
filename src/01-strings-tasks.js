7fedb8d708c25105cf581207b2d1410aa41f1ceb
artemmyzrov <a.myzrov@yandex.ru>
2023-03-02 17:55:54 +0400

    feat : 2 task compled

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -32,8 +32,8 @@ function concatenateStrings(a, b) {
  *   'b'     => 1
  *   ''      => 0
  */
-function getStringLength(/* value */) {
-  throw new Error('Not implemented');
+function getStringLength(a) {
+  return a.length;
 }
 
 /**
@@ -49,8 +49,8 @@ function getStringLength(/* value */) {
  *   'John','Doe'      => 'Hello, John Doe!'
  *   'Chuck','Norris'  => 'Hello, Chuck Norris!'
  */
-function getStringFromTemplate(/* firstName, lastName */) {
-  throw new Error('Not implemented');
+function getStringFromTemplate(firstName, lastName) {
+  return `Hello, ${firstName} ${lastName}!`;
 }
 
 /**
