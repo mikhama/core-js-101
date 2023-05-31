af43510b0e155d8f844c460b6919bc87d56637b7
AliaksandrButsin <aliaksandrbutsin@gmail.com>
2023-05-31 18:04:29 +0200

    add: string-solution-07

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -118,8 +118,9 @@ function removeLeadingAndTrailingWhitespaces(value) {
  *   'A', 5  => 'AAAAA'
  *   'cat', 3 => 'catcatcat'
  */
-function repeatString(/* value, count */) {
-  throw new Error('Not implemented');
+function repeatString(value, count) {
+  // throw new Error('Not implemented');
+  return value.repeat(count);
 }
 
 /**
