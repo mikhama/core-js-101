c8ee2352c6ef809379e8f0c8c4257f4ed6f502c4
AliaksandrButsin <aliaksandrbutsin@gmail.com>
2023-06-01 12:24:42 +0200

    add: numbers-solution-02

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -35,8 +35,9 @@ function getRectangleArea(width, height) {
  *   3.14 => 19.729201864543903
  *   0    => 0
  */
-function getCircleCircumference(/* radius */) {
-  throw new Error('Not implemented');
+function getCircleCircumference(radius) {
+  // throw new Error('Not implemented');
+  return 2 * Math.PI * radius;
 }
 
 /**
